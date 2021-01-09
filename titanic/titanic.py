97d67c3ea583be528c4416dc7ed72135a52fe23b
Emin Carbas <carbasemin@gmail.com>
2021-01-09 07:43:09 +0300

    added some comments to titanic.py.

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import GridSearchCV
 data = pd.read_csv('./train.csv', index_col='PassengerId')
 test = pd.read_csv('./test.csv', index_col='PassengerId')
 
+# Cabin, Name and Ticket fetures are not that relevant. So, drop 'em.
 X = data.drop(columns=['Cabin', 'Name', 'Ticket'])
 y = X.pop('Survived')
 
@@ -20,17 +21,19 @@ test = test.drop(columns=['Cabin', 'Name', 'Ticket'])
 categorical_cols = [col for col in X.columns if
                    X[col].dtype == 'object']
 
-# First, impute the missing values with the most frequent value of the feature,
-# then, onehotencode stuff. Basic.
 categorical_transformer = Pipeline(
 	steps=[
 		('imputer', SimpleImputer(strategy='most_frequent')),
 		('onehot', OneHotEncoder(sparse=False))
 		])
 
+# A custom transformer that combines SibSp and Parch to create a new 
+# feature called is_alone. It drops those two features after adding the new one.
 alone = Alone()
 
-# So I can't get this stuff to work with Alone. Shit. I'll figure it out.
+# Filling the missing values with the median value (for numerical values, obviously),
+# adding the new feature is_alone,
+# and encoding categorical features after handling their missing values. 
 transformer = ColumnTransformer(
 	transformers=[
 		('num', SimpleImputer(strategy='median'), ['Pclass', 'Fare', 'Age']),
@@ -44,11 +47,11 @@ model = Pipeline(
 		('rf', RandomForestClassifier())
 	])
 
-param_grid = { 
+param_grid = {
     'rf__n_estimators': [10, 50, 100],
     'rf__max_features': ['auto', 'sqrt', 'log2'],
-    'rf__max_depth' : [4,5,6,7,8],
-    'rf__criterion' :['gini', 'entropy']
+    'rf__max_depth': [4, 5, 6, 7, 8],
+    'rf__criterion': ['gini', 'entropy']
 }
 
 clf = GridSearchCV(model, param_grid=param_grid, n_jobs=-1, cv=5)
@@ -57,7 +60,7 @@ clf.fit(X, y)
 
 best_params, best_score = clf.best_params_, clf.best_score_
 
-# To get rid of the "rf__"
+# To get rid of the "rf__". Check out param_grid above if this sounds odd.
 keys = [key.split('__')[1] for key in best_params.keys()]
 values = list(best_params.values())
 
